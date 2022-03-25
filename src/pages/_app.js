9bc204f956e379a1e96ef51d851755c9a0ac05db
amorandi <a.morandi.info@gmail.com>
2022-03-25 17:11:12 +0100

    updated infura provider configuration

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -15,15 +15,8 @@ import { providers } from 'ethers'
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 import { WalletLinkConnector } from 'wagmi/connectors/walletLink'
 // API key for Ethereum node
-// Two popular services are Infura (infura.io) and Alchemy (alchemy.com)
-//const infuraId = 'https://polygon-mainnet.g.alchemy.com/v2/-rPx9b6dd_H7M-2pTA8f0YckHKuYyB1B'
-//const infuraId = 'https://eth-mainnet.alchemyapi.io/v2/HN-971sXOyKC7_PTkym_48bZm2aRxfPO'
-//const infuraId = 'https://mainnet.infura.io/v3/75cdb70ef36741d99a8ec2f0db18843b'
-//const infuraId = 'https://rinkeby.infura.io/v3/67a611b07570432e9d77b5dfaa2d2ccb'
-//const infuraIdPolygon = 'https://polygon-mainnet.infura.io/v3/738ad3dab142418bb9651f43a54e6c30'
-//const infuraIdMumbai = 'https://polygon-mumbai.infura.io/v3/738ad3dab142418bb9651f43a54e6c30'
-const infuraIdPolygon = 'd15d18ba6d8b43ff9bfbe7e3cf6dbb41'
-const infuraIdMumbai = 'd15d18ba6d8b43ff9bfbe7e3cf6dbb41'
+
+const infuraId = process.env.NEXT_PUBLIC_INFURA_NODE_PROVIDER
 
 
 // Chains for connectors to support
@@ -33,17 +26,6 @@ const connectors = ({ chainId }) => {
   const rpcUrl =
     chains.find((x) => x.id === chainId)?.rpcUrls?.[0] ??
     chain.mainnet.rpcUrls[0]
-  /*
-  let i ;
-  if (chainId === 80001) {
-    i = infuraIdMumbai
-  } else if (chainId === 137) {
-    i = infuraIdPolygon
-  } else {
-    i = infuraIdMumbai
-  }*/
-  //const infuraId = infuraIdMumbai
-
 
 
   return [
@@ -51,7 +33,7 @@ const connectors = ({ chainId }) => {
       chains,
       options: { shimDisconnect: true },
     }),
-   /* new WalletConnectConnector({
+  /*  new WalletConnectConnector({
       options: {
         infuraId,
         qrcode: true,
@@ -71,14 +53,8 @@ const connectors = ({ chainId }) => {
 // Use `chainId` to make ethers.js provider network-aware
 const provider = ({ chainId }) =>{  
   
-  if (chainId=== 80001 ) 
-  {
-   return  new providers.InfuraProvider(chainId, infuraIdMumbai)
-  }
-  if ( chainId ===137 ) {
-    return   new providers.InfuraProvider(chainId, infuraIdPolygon)
-  }
 
+  return  new providers.InfuraProvider(chainId, infuraId)
 }
 
 function Website({ Component, pageProps, router }) {
